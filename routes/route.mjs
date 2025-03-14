ca60f7503bb753aeeff72d55648e3bc2db02ae92
yao <parfaitericyao123@gmail.com>
2025-03-14 19:07:30 +0000

    test8

--- routes/route.mjs
+++ routes/route.mjs
@@ -1,12 +1,4 @@
 import express from "express"
-import { CreatePatient, DeletePatient, GetPatient, UpdatePatient } from "../controllers/patient.controller.mjs"
-import { CreateDossier, GetDossier, UpdateDossier } from "../controllers/dossierMedical.controller.mjs"
-import { CreateRendezvous, GetRendezvous, UpdateRendezvous } from "../controllers/rendezvous.controller.mjs"
-import { CreateExamenMedicaux, GetExamenMedicaux, UpdateExamenMedicaux } from "../controllers/examenMedicaux.controller.mjs"
-import { CreateContactUrgence, GetContactUrgence, UpdateContactUrgence } from "../controllers/contactUrgence.controller.mjs"
-import { CreateHospitalisations, GetHospitalisations, UpdateHospitalisations } from "../controllers/hospitalisation.controller.mjs"
-import { CreateMaladieChronic, GetMaladieChronic, UpdateMaladieChronic } from "../controllers/maladiChronique.controller.mjs"
-import { CreateStatistiquePatient, GetStatistiquePatient, UpdateStatistiquePatient } from "../controllers/statisticPatient.controller.mjs"
 
 const router = express.Router()
 
@@ -14,6 +6,5 @@ const router = express.Router()
 router.get("/get-patient/", (req, res) => {
     res.json("Salut je suis une route qui marche très bien")
 })
-
 
 export default router
