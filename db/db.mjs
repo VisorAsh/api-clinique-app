656454db9c4fbe757d55dab925f8d22313e69ee7
yao <parfaitericyao123@gmail.com>
2025-03-15 18:21:39 +0000

    test 23

--- db/db.mjs
+++ db/db.mjs
@@ -2,18 +2,19 @@ import mongoose from "mongoose"
 
 export const MongoConnected = async () => {
     try {
+        const atlas = process.env.mongoAtlas
 
-        const url = process.env.mongoURI
         /* 
-         const atlas = process.env.mongoAtlas
+         const url = process.env.mongoURI
         
          const mongoCompass = process.env.mongoCompass */
-        await mongoose.connect(url, {
+        await mongoose.connect(atlas, {
             dbName: "DB_CLINIC"
         })
         console.log("Connexion à la base de donnée réussie avec succès !")
         return "ok"
     } catch (error) {
         console.log("Erreur de connexion à", error)
+        return "Problème de connexion à la bd"
     }
 }
--- index.mjs
+++ index.mjs
@@ -7,7 +7,6 @@ import { MongoConnected } from "./db/db.mjs"
 import cors from "cors"
 
 const app = express()
-MongoConnected()
 
 //Pour donner l'autorisation à des domaines sur votre serveur
 //const domaineAutorise = ["http://localhost:3500", "https://frontend-login-rho.vercel.app/"]
@@ -26,8 +25,10 @@ app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use("/api", router)
-app.get("/", (req, res) => {
-    res.send("Bienvenue sur notre app clinic")
+app.get("/", async (req, res) => {
+    const re = await MongoConnected()
+    if (res === "ok") res.send(res)
+    else res.send("Erreur de connexion à la bd")
 })
 
 app.listen(3600, () => console.log("Serveur démarré avec succès sur le port 3600"))
