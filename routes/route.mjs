c625d306e960af22a8c1ada14d9a6a47a7faba63
yao <parfaitericyao123@gmail.com>
2025-03-16 15:02:34 +0000

    test 32

--- routes/route.mjs
+++ routes/route.mjs
@@ -7,7 +7,7 @@ import { CreateMaladieChronic, DeleteMaladieChronic, GetMaladieChronic, UpdateMa
 import { CreateRendezvous, DeleteRendezvous, GetRendezvous, UpdateRendezvous } from "../controllers/rendezvous.controller.mjs"
 import { CreateStatistiquePatient, DeleteStatistiquePatient, GetStatistiquePatient, UpdateStatistiquePatient } from "../controllers/statisticPatient.controller.mjs"
 import { CreateUser, DeleteUser, GetUser, UpdateUser } from "../controllers/usersController.mjs"
-import { CreateDossier, GetDossier, UpdateDossier } from "../controllers/dossierMedical.controller.mjs"
+import { CreateDossier, DeleteDossier, GetDossier, UpdateDossier } from "../controllers/dossierMedical.controller.mjs"
 /*
 
 
@@ -33,6 +33,7 @@ router.post("/create-user", CreateUser)
 /****************** Routes Dossier Medical Patient  *****************/
 router.get("/get-dossier/:_id", GetDossier)
 router.put("/update-dossier/:_id", UpdateDossier)
+router.delete("/delete-dossier/:_id", DeleteDossier)
 router.post("/create-dossier", CreateDossier)
 
 /****************** Routes Rendez vous Patient  *****************/
