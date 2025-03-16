6e5e73d24e59f6e60d1bef2bdb4ade6f05eb91ae
yao <parfaitericyao123@gmail.com>
2025-03-16 15:05:14 +0000

    test 33

--- models/dossierMedical.model.mjs
+++ models/dossierMedical.model.mjs
@@ -3,9 +3,25 @@ const { model, Schema } = mongoose
 
 const DossierMedicalSchema = new Schema({
     antecedentsMedicaux: [{ type: String }],
-    consultations: [{ type: String }],
-    prescriptions: [{ type: String }],
-    hospitalisations: [{ type: String }]
+    consultations: [{
+        date: { type: Date, default: Date.now },
+        motif: { type: String, required: true },
+        diagnostic: { type: String, required: true },
+        traitement: { type: String, required: true },
+        medecin: { type: String, required: true }
+    }],
+    prescriptions: [{
+        date: { type: Date, default: Date.now },
+        medicaments: [String],
+        instructions: { type: String }
+    }],
+    hospitalisations: [{
+        dateDebut: { type: Date, required: true },
+        dateFin: { type: Date, required: true },
+        motif: { type: String, required: true },
+        hopital: { type: String, required: true },
+        traitements: { type: String, required: true }
+    }]
 })
 
 export const DossierMedicalModel = model.DossierMedicalElectronic || model("DossierMedicalElectronic", DossierMedicalSchema)
