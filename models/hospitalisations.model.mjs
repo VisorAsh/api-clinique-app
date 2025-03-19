e739495d0f2861091623c2b85f4748f80f9a7804
yao <parfaitericyao123@gmail.com>
2025-03-19 13:52:24 +0000

    test 4

--- models/hospitalisations.model.mjs
+++ models/hospitalisations.model.mjs
@@ -3,20 +3,18 @@ const { model, Schema } = mongoose
 
 const HospitalisationsSchema = new Schema({
     patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patients", required: true },
-    medecin: { type: String ,required:true},
-    lieu: { type: String,required:true },
+    nomPatient: { type: String, required: true },
+    medecin: { type: String, required: true },
+    chambre: { type: String, required: true },
     dateAdmission: { type: Date, required: true },
     dateSortie: { type: Date },
     motif: { type: String, required: true },
-    traitements: { type: [String], default: [] },
-    interventionsChirurgicales: { type: [String], default: [] },
-    resumeSuivi: { type: String },
-    recommandations: { type: [String], default: [] },
     statut: {
         type: String,
         enum: ["en cours", "terminée", "annulée"],
         default: "en cours"
-    }
+    },
+    symptoms: { type: String, required: false }
 })
 
 export const HospitalisationsModel = model.Hospitalisations || model("Hospitalisations", HospitalisationsSchema)
