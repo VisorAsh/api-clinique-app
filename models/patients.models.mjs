aaa6a9e3d84d0916bf323492c470d39a6de495ce
yao <parfaitericyao123@gmail.com>
2025-03-19 14:09:03 +0000

    test 45

--- models/patients.models.mjs
+++ models/patients.models.mjs
@@ -4,10 +4,10 @@ const { model, Schema } = mongoose
 const InfosPatientsSchema = new Schema({
     nom: { type: String, required: true },
     prenom: { type: String, required: true },
-    dateNaissance: { type: Date, required: true },
-    contact: { type: String, required: true },
+    birthDate: { type: Date, required: true },
+    telephone: { type: String, required: true },
     email: { type: String },
-    sexe: { type: String, required: true },
+    gender: { type: String, required: true },
     createdAt: { type: Date, default: Date.now() }
 })
 
